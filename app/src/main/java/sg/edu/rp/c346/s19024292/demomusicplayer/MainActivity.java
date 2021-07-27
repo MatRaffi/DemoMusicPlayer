a415d4346d10f07e60553a518bcd7c8f31664370
Mat <19024292@rp.edu.sg>
2021-07-27 16:28:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/s19024292/demomusicplayer/MainActivity.java
@@ -0,0 +1,40 @@
+package sg.edu.rp.c346.s19024292.demomusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] permission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions(MainActivity.this, permission, 0);
+
+        start = findViewById(R.id.btnStart);
+        stop = findViewById(R.id.btnStop);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+    }
+}
